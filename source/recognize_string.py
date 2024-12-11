fea8115a4eec5f795c18fd5d547a63d7189ca1bd
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2024-12-11 14:11:09 -0300

    🧪 Added recognize string function (in testing)

--- source/recognize_string.py
+++ source/recognize_string.py
@@ -1,5 +1,24 @@
 from generate_string_multi import read_archive, generate_string, axiom
 
+def recognize_string_2(length, axiom, rules, string):
+    stack = [('$',0),(axiom, 0)]
+    charPosition = 0
+    
+    while charPosition < len(string):
+        char = string[charPosition]
+        topChar, gen = stack.pop()
+        
+        if topChar == '$': 
+            return False
+        if topChar in rules and gen != length:
+            stack.extend((newChar,gen+1) for newChar in rules[topChar][0][::-1])
+        elif topChar == char:
+            charPosition += 1
+        else:
+            return False
+        
+    return stack.pop() == ('$', 0)        
+
 def recognize_string(length, axiom, rules, string) -> bool:
     stack = [('$',0),(axiom, 0)]
     charPosition = 0
@@ -32,5 +51,5 @@ def recognize_string(length, axiom, rules, string) -> bool:
 if __name__ == "__main__":
     file = read_archive("rules.json")
     string = generate_string(file["length"], file["rules"])[0]
-    print(string)
-    print(recognize_string(file["length"], axiom, file["rules"], string))
+    string = string[:15] + 'X' + string[15:]
+    print(recognize_string_2(file["length"], axiom, file["rules"], string))
