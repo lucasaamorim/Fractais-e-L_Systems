209f1cfaa2eb21db21abfffda0c184db58ab1b8b
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2024-12-11 10:30:18 -0300

    🐛 Added bugs

--- source/recognize_string.py
+++ source/recognize_string.py
@@ -1,33 +1,36 @@
-def recognize_string(length, rules, string) -> bool:
-    stack = ['$']
+from generate_string_multi import read_archive, generate_string, axiom
 
-    for char in string:
-        topChar = stack[-1]
-        if topChar not in terminals:
-
-        elif topChar == char:
-            stack.pop()
-        else:
-            return False
-
-    return True
-
-def recursive_call(rules,string):
+def recognize_string(length, axiom, rules, string) -> bool:
+    stack = [('$',0),(axiom, 0)]
+    charPosition = 0
+    tmpString = ''
+    #TODO: Remover isso, so p debug
+    returnValue = True
     
+    while charPosition < len(string):
+        char = string[charPosition]
+        topChar, gen = stack.pop()
+        
+        tmpString += topChar if length == gen else ''
+           
+        if topChar == '$': 
+            returnValue = False
+            break
+        elif topChar == char and length == gen:
+            charPosition += 1
+        elif topChar in rules and gen != length:
+            stack.extend((newchar,gen+1) for newchar in rules[topChar][0][::-1])
+        elif gen == length:
+            returnValue = False
+    
+    print(tmpString)
+    return stack.pop() == ('$', 0) and returnValue
 
 
-def recognize_string_2(length, rules, string) -> bool:
-    stacks = []
-    for i in length(rules):
-        stacks.append([rules[]])
-
-    for char in string:
-        topchar = stack[-1]
-        if topchar not in terminals:
-
-        elif topchar == char:
-            stack.pop()
-        else:
-            return False
-
-    return True
+#def recursive_call(rules,string):
+    
+if __name__ == "__main__":
+    file = read_archive("rules.json")
+    string = generate_string(file["length"], file["rules"])[0]
+    print(string)
+    print(recognize_string(file["length"], axiom, file["rules"], string))
