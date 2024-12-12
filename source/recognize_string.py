f721ef290741bccabe07906082857818aff6077e
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2024-12-12 20:05:10 -0300

    make new function the definitive version

--- source/recognize_string.py
+++ source/recognize_string.py
@@ -1,6 +1,6 @@
 from generate_string_multi import read_archive, generate_string, axiom
 
-def recognize_string_2(length, axiom, rules, string):
+def recognize_string(length, axiom, rules, string):
     stack = [('$',0),(axiom, 0)]
     charPosition = 0
     
@@ -18,33 +18,6 @@ def recognize_string_2(length, axiom, rules, string):
             return False
         
     return stack.pop() == ('$', 0)        
-
-def recognize_string(length, axiom, rules, string) -> bool:
-    stack = [('$',0),(axiom, 0)]
-    charPosition = 0
-    tmpString = ''
-    #TODO: Remover isso, so p debug
-    returnValue = True
-    
-    while charPosition < len(string):
-        char = string[charPosition]
-        topChar, gen = stack.pop()
-        
-        tmpString += topChar if length == gen else ''
-           
-        if topChar == '$': 
-            returnValue = False
-            break
-        elif topChar == char and length == gen:
-            charPosition += 1
-        elif topChar in rules and gen != length:
-            stack.extend((newchar,gen+1) for newchar in rules[topChar][0][::-1])
-        elif gen == length:
-            returnValue = False
-    
-    print(tmpString)
-    return stack.pop() == ('$', 0) and returnValue
-
 
 #def recursive_call(rules,string):
     
