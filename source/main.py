15ae82b39735f28b0a7d426eb79e12fed9511cc4
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2024-12-14 08:10:34 -0300

    ruledict declaration on the right place

--- source/main.py
+++ source/main.py
@@ -5,14 +5,14 @@ from turtle_main import create_screen, create_turtle
 
 if __name__ == "__main__":
   file = sys.argv[1]
+  ruledict = read_archive(file)
   
   if len(sys.argv) == 5 and sys.argv[2] == "--verificar":
     string = sys.argv[3]
     #Axiom = X always??
     depth = sys.argv[4]
-    print(recognize_string(depth, 'X', read_archive(file)["rules"], string))
+    print(recognize_string(depth, ruledict["rules"]['X'], ruledict["rules"], string))
   else:
-    ruledict = read_archive(file)
     screen = create_screen()
     bob = create_turtle()
     screen.mainloop()
